5a6a38cc41e44453fc51e942a9ef76efffa311a7
Luis Cordova <luis.cordova.garrido@gmail.com>
2023-03-30 20:10:31 -0300

    criptos react

---
+++ src/data/monedas.js
@@ -0,0 +1,11 @@
+
+const monedas = [
+    { id: 'USD', nombre: 'Dolar de Estados Unidos'},
+    { id: 'EUR', nombre: 'Euro'},
+    { id: 'JPY', nombre: 'Yen Japones'},
+    { id: 'CLP', nombre: 'Peso Chileno'},
+]
+
+export {
+    monedas
+}
